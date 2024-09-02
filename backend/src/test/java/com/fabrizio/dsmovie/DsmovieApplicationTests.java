cfdd07164d7f5d770ffa3b2e18b5d3c27e17fcfa
Fabrizio Abreu <42725809+fabrizioabreu@users.noreply.github.com>
2024-09-02 18:45:51 -0300

    Projeto criado

---
+++ backend/src/test/java/com/fabrizio/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrizio.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
